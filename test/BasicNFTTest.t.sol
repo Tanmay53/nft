673e192262d23877ba2d8723712b3086104f6481
Tanmay53 <tanmaykhatri53@gmail.com>
2023-08-20 01:43:41 +0530

    Basic NFT completed with deploy script and tests. Added Makefile

---
+++ test/BasicNFTTest.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import { Test } from "forge-std/Test.sol";
+import { DeployBasicNFT } from "../script/DeployBasicNFT.s.sol";
+import { BasicNFT } from "../src/BasicNFT.sol";
+
+contract BasicNFTTest is Test {
+    DeployBasicNFT public deployer;
+    BasicNFT public basicNFT;
+    address private s_user = makeAddr("user");
+    string public constant PUG = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function setUp() public {
+        deployer = new DeployBasicNFT();
+        basicNFT = deployer.run();
+    }
+
+    function testNameIsCorrect() public view {
+        string memory expectedName = "Dogie";
+        string memory actualName = basicNFT.name();
+
+        // assertEq(expectedName, actualName);
+        assert(keccak256(abi.encodePacked(expectedName)) == keccak256(abi.encodePacked(actualName)));
+    }
+
+    function testCanMintAndHaveBalance() public {
+        vm.prank(s_user);
+        basicNFT.mintNFT(PUG);
+
+        assert(basicNFT.balanceOf(s_user) == 1);
+        assert(keccak256(abi.encodePacked(PUG)) == keccak256(abi.encodePacked(basicNFT.tokenURI(0))));
+    }
+}
